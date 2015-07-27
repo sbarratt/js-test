7de96e86f54ab8da073d41123bd743f40f227a56
Shane Barratt <stbarratt@gmail.com>
2015-07-26 21:14:45 -0400

    added default test

--- test.js
+++ test.js
@@ -40,26 +40,7 @@ tests = function() {
 
 	// Include Tests to Run here
 
-	generic = "Hello my name is Shane.";
-	doublespace = "How are you?  I am doing very well.";
-	allthree = "This is a long sentence. This is a longer sentence! Kind of?";
-	word = "word";
-
-	generic = wc.getStats(generic);
-	doublespace = wc.getStats(doublespace);
-	allthree = wc.getStats(allthree);
-	word = wc.getStats(word);
-
-	checkStats = function(a, values, label) {
-		test(a.characters, values[0], label + "- Characters");
-		test(a.words, values[1], label + "- Words");
-		test(a.sentences, values[2], label + "- Sentences");
-	};
-
-	checkStats(generic, [23,5,1], "Generic");
-	checkStats(doublespace, [35,8,2], "Double Space");
-	checkStats(allthree, [60,12,3], "All Three");
-	checkStats(word, [4,1,0], "Single Word");
+	test(1, 1, "1=1?");
 
 
 	countPassFail();
